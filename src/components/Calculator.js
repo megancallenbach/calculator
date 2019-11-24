4bad80644b7a7b11484079bec2e96a1c32f19294
Megan Callenbach <megan@brightfish.nl>
2019-11-24 09:36:01 +0100

    Added calculation function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 
-const numericKeys = _.range(10).reverse();
+const numberKeys = [
+  { value: 0, order: 18 },
+  { value: 1, order: 13 },
+  { value: 2, order: 14 },
+  { value: 3, order: 15 },
+  { value: 4, order: 9 },
+  { value: 5, order: 10 },
+  { value: 6, order: 11 },
+  { value: 7, order: 5 },
+  { value: 8, order: 6 },
+  { value: 9, order: 7 },
+  { value: '.', order: 17 },
+];
 
-const controlKeys = ['+', '-', 'x', '/', '√'];
+const mathKeys = [
+  { value: '+', order: 2 },
+  { value: '-', order: 3 },
+  { value: '/', order: 4 },
+  { value: 'x', order: 8 },
+  { value: '^', order: 12 },
+  { value: '√', order: 16 },
+];
 
 const Button = ({ value, onPress, order, flex }) => {
   return (
-    <div className="btn btn-simple" style={{order: order, flex: flex}} onClick={onPress}>
+    <div className="btn" style={{order: order, flex: flex}} onClick={onPress}>
       {value}
     </div>
   );
@@ -28,12 +47,41 @@ class Calculator extends Component {
 
     let calculation = this.state.calculation;
 
-    if (calculation === '0' && numericKeys.indexOf(value) !== -1) {
+    let isNumberKey = _.findIndex(numberKeys, (obj) => obj.value === value) !== -1;
+    let isMathKey = !isNumberKey && _.findIndex(mathKeys, (obj) => obj.value === value) !== -1;
+
+
+    if (calculation === 'Infinity') {
+      calculation = '0';
+      value = '';
+
+      // remove leading 0
+    } else if (calculation === '0' && (isNumberKey || value === '√' || value === '-')) {
       calculation = '';
+
+      // starting with a math key unsupported...
     } else if (calculation === '0') {
       value = '';
-    } else if (controlKeys.indexOf(calculation.slice(-1)) !== -1 && controlKeys.indexOf(value) !== -1) {
-      calculation = calculation.slice(0, -1);
+
+      // double math key replaces first one
+    } else if (_.findIndex(mathKeys, (obj) => obj.value === calculation.slice(-1)) !== -1 && isMathKey) {
+
+      let isAllowed = _.findIndex(mathKeys, (obj) => obj.value === calculation.slice(-2, -1)) === -1 && (value === '-' || (value === '√' && calculation.slice(-1) === '-'));
+
+      if (!isAllowed) {
+        calculation = calculation.slice(0, -1);
+      }
+
+      // no double decimals allowed
+    } else if (value === '.') {
+
+      let reversedCalc = calculation.split("").reverse().join("");
+      let numberRegex = '^(?<number>[-]?[\\d]*[.]?[\\d]*(e+\\d*)?)';
+      let numberMatch = reversedCalc.match(numberRegex);
+
+      if (numberMatch && numberMatch['groups']['number'].indexOf('.') !== -1) {
+        value = '';
+      }
     }
 
     calculation = calculation + value;
@@ -42,6 +90,106 @@ class Calculator extends Component {
   }
 
   calculate() {
+
+    let calculation = this.state.calculation;
+
+    let numbers = [];
+    let operators = [];
+
+    let numberRegex = '^(?<number>[-]?[\\d]*[.]?[\\d]*(e\\+\\d*)?)';
+    let operatorRegex = '^(?<operator>[+\\-x/^√])';
+
+    let findNumber = () => {
+      let numberMatch = calculation.match(numberRegex);
+
+      if (numberMatch && numberMatch['groups']['number']) {
+        numbers.push(numberMatch['groups']['number']);
+
+        calculation = calculation.substring(numberMatch['groups']['number'].length);
+      }
+    }
+
+    let findOperator = () => {
+      let operatorMatch = calculation.match(operatorRegex);
+
+      if (operatorMatch && operatorMatch['groups']['operator']) {
+        operators.push(operatorMatch['groups']['operator']);
+
+        calculation = calculation.substring(operatorMatch['groups']['operator'].length);
+      }
+    }
+
+    while (calculation.length !== 0) {
+      findNumber();
+      findOperator();
+    }
+
+    while (operators.length !== 0) {
+
+      let orderOfOperations = [
+        ['^', '√'],
+        ['x', '/'],
+        ['+', '-'],
+      ];
+
+      orderOfOperations.forEach((prioOperators) => {
+
+        let index = _.findIndex(operators, (op) => prioOperators.indexOf(op) !== -1);
+
+        while (index !== -1) {
+
+          let result = null;
+
+          let operator = operators[index];
+
+          operators.splice(index, 1);
+
+          let firstNumber = numbers[index];
+          let secondNumber = numbers[index + 1];
+
+          numbers.splice((index), 1);
+
+          if (firstNumber != null && operator && secondNumber !== null) {
+
+            switch (operator) {
+              case 'x':
+                result = Number(firstNumber) * Number(secondNumber);
+                break;
+
+              case '/':
+                result = Number(firstNumber) / Number(secondNumber);
+                break;
+
+              case '^':
+                result = Math.pow(Number(firstNumber), Number(secondNumber));
+                break;
+
+              case '+':
+                result = Number(firstNumber) + Number(secondNumber);
+                break;
+
+              case '-':
+                result = Number(firstNumber) - Number(secondNumber);
+                break;
+
+              case '√':
+                result = Number(firstNumber) * Math.sqrt(Number(secondNumber));
+                break;
+            }
+          } else if (operator === '√' && firstNumber) {
+            result = Math.sqrt(Number(firstNumber));
+          }
+
+          numbers[index] = result;
+
+          index = _.findIndex(operators, (op) => prioOperators.indexOf(op) !== -1);
+
+        }
+      });
+
+    }
+
+    this.setState({ calculation: numbers[0].toString() });
 
   }
 
@@ -56,44 +204,32 @@ class Calculator extends Component {
               {this.state.calculation}
             </div>
             <div className="buttons">
-              <div className="numeric-buttons">
-                { numericKeys.map((btn) => (
+
+                { numberKeys.map((btn) => (
                   <Button
-                    value={btn.toString()}
-                    order={btn === 0 ? 3 : 1}
-                    onPress={() => this.appendValue(btn)}
+                    value={btn.value.toString()}
+                    order={btn.order}
+                    onPress={() => this.appendValue(btn.value)}
+                  />
+                ))}
+                { mathKeys.map((btn) => (
+                  <Button
+                    value={btn.value.toString()}
+                    order={btn.order}
+                    onPress={() => this.appendValue(btn.value)}
                   />
                 ))}
                 <Button
-                  value=','
-                  order={2}
-                  onPress={() => this.appendValue(',')}
-                />
-                <Button
                   value='AC'
-                  order={4}
+                  order={1}
                   onPress={() => this.setState({ calculation: '0'})}
                 />
                 <Button
-                  value={'^'}
-                  order={5}
-                  onPress={() => this.calculate()}
-                />
-                <Button
-                  value={'='}
-                  order={6}
+                  value='='
+                  order={19}
                   flex={2}
                   onPress={() => this.calculate()}
                 />
-              </div>
-              <div className="control-buttons">
-                { controlKeys.map((btn) => (
-                  <Button
-                    value={btn.toString()}
-                    onPress={() => this.appendValue(btn)}
-                  />
-                ))}
-              </div>
             </div>
           </div>
         </div>
