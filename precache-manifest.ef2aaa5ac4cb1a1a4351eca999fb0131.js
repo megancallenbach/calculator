5d2d9015c2066a38f73c6d0935df5acedfaf887e
Megan Callenbach <megan@brightfish.nl>
2019-11-24 12:45:32 +0100

    Updates

---
+++ precache-manifest.ef2aaa5ac4cb1a1a4351eca999fb0131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713467cd901565cc87eda97f064a67cd",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "67e882f26353fc15db56",
+    "url": "/calculator/static/css/main.83d010f3.chunk.css"
+  },
+  {
+    "revision": "a76ee971f61b35b4df78",
+    "url": "/calculator/static/js/2.a2a835b0.chunk.js"
+  },
+  {
+    "revision": "67e882f26353fc15db56",
+    "url": "/calculator/static/js/main.c26d0e0f.chunk.js"
+  },
+  {
+    "revision": "f69b5c5b50f5b7cd07bd",
+    "url": "/calculator/static/js/runtime-main.736c2c55.js"
+  }
+]);
